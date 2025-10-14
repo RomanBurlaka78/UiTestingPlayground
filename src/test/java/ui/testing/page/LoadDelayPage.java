d7f8c4b9f622acd7a5985baaa936f45d9b6948a6
Roman Burlaka <burlakarv@gmail.com>
2025-10-14 20:18:24 +0200

    testLoadDelay for FirstRowTest

---
+++ src/test/java/ui/testing/page/LoadDelayPage.java
@@ -0,0 +1,22 @@
+package ui.testing.page;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import ui.testing.base.BasePage;
+
+public class LoadDelayPage extends BasePage {
+    public LoadDelayPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("Wait until button will be presence on page")
+    public String confirmAppearedButton() {
+        WebElement btnAppeared = getDriver().findElement(By.xpath("//button[@class = 'btn btn-primary']"));
+        wait4().until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//button[@class = 'btn btn-primary']")));
+
+        return btnAppeared.getText();
+    }
+}
